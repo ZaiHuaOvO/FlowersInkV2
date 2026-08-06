4a549b39b67a0327a44d2b55cee045ea512c47e7
ZFFFLower <yzy@luodedao.com>
2026-08-06 10:27:42 +0800

    docs(about): 更新建站历程中装备图鉴上线日期
    - 新增装备图鉴模块日期 2026/07/24 → 2026/08/04

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -187,7 +187,7 @@ export class AboutComponent implements AfterViewInit, OnDestroy {
     { date: '2026/04/28', title: '花墨真正接入了 CDN', text: '以前一直接错了！' },
     { date: '2026/07/15', title: '新增博客和点滴评论功能', text: '为网站加上一些交互感' },
     { date: '2026/07/24', title: '重写了一个可爱的欢迎页', text: '' },
-    { date: '2026/07/24', title: '新增装备图鉴模块', text: '记录一下我的老朋友们' },
+    { date: '2026/08/04', title: '新增装备图鉴模块', text: '记录一下我的老朋友们' },
     { date: '未完待续', title: '', text: '' },
   ];
 
